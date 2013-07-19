cec68d2c8ad9c0932e08571dcfe466b47765e823
Saymon <Saymon@192.168.1.2>
2013-07-19 01:49:30 -0300

    formapag e statuspedido mais completo, falta mexer em pedidoaction.

--- DAO/connection.php
+++ DAO/connection.php
@@ -2,7 +2,7 @@
 class connection{
 private static $connection = NULL;
 function conecta() {	
-	$conexao = new mysqli("127.0.0.1", "root", "root","lolwtf2") or die("Problem connecting: ".mysqli_error());        
+	$conexao = new mysqli("127.0.0.1", "root", "","lolwtf2") or die("Problem connecting: ".mysqli_error());        
 	return $conexao;
 }
 public static function getInstance() {
